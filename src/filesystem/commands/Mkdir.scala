7cef4f8682e3b5f8d021540452d144bae1f82368
Ben Drawer <benjamin.drawer@tray.io>
2020-06-03 20:11:03 +0100

    Minor refactor of Mkdir

--- src/filesystem/commands/Mkdir.scala
+++ src/filesystem/commands/Mkdir.scala
@@ -6,13 +6,7 @@ import filesystem.filesystem.State
 class Mkdir(val name: String) extends Command {
   override def apply(state: State): State = State(
     state.root,
-    Directory.addItemToDirectory(
-      state.wd,
-      Directory.createEmpty(
-        s"${state.wd.parentPath}/${state.wd.name}",
-        name
-      )
-    ),
+    Directory.addEmpty(state.wd, name),
     s"Created directory: $name"
   )
 }
--- src/filesystem/files/Directory.scala
+++ src/filesystem/files/Directory.scala
@@ -7,15 +7,17 @@ object Directory {
   val SEPARATOR = "/"
   val ROOT_PATH = "/"
 
-  def addItemToDirectory(directory: Directory, item: Item): Directory = new Directory(
-    directory.parentPath,
-    directory.name,
-    directory.contents :+ item
-  )
   def createEmpty(parentPath: String, name: String): Directory = new Directory(
     parentPath,
     name,
     List()
   )
+
   def createRoot: Directory = Directory.createEmpty("", "")
+
+  def addEmpty(parentDirectory: Directory, name: String): Directory =
+    Directory.createEmpty(
+      s"${parentDirectory.parentPath}/${parentDirectory.name}",
+      name
+    )
 }
